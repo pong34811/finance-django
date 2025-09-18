b947a657381ddd31a50a1e3a3b78aa8342813045
Waritnan <pong34811@gmail.com>
2025-09-18 17:32:14 +0700

    feat: Update database settings to use PostgreSQL with Supabase configuration

--- backend/settings.py
+++ backend/settings.py
@@ -82,13 +82,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # ตาม -d postgres
+        'USER': 'postgres.hoqqifqfrrmsfqzsuklq',  # ตาม -U postgres
+        'PASSWORD': 'e2ftGbWy7S4hDrSV',
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
