858799ac67f39e5312c0b6d1de40ebd519095d3b
Waritnan <pong34811@gmail.com>
2025-09-18 17:20:59 +0700

    fix: Correct WSGI settings module path to 'backend.settings'

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -11,9 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-
 
 app = application
